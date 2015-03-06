212af948accd499b4996f11660bc1bc35eafbbb2
jerrmhs <jerrmhs@gmail.com>
2015-03-06 09:11:19 -0800

    added test data for autofill

--- PageController.js
+++ PageController.js
@@ -3,4 +3,38 @@ angular.module('trainingScheduler').controller('PageController', ['$http', '$sco
 	$data.read.grabRequests().then(function(resp) {
 		$scope.requests = resp.results;
 	});
+	$scope.emp = [
+		"John Smith",
+		"Micky Mouse",
+		"Luke Skywalker",
+		"John Shapered",
+		"Gordon Freemen",
+		"Lele Freewalker",
+		"Jessie SeeFar",
+		"Tarzan Jungal`Lard",
+		"Jerry Schossow",
+		"Nick Schleder",
+		"Ahn Tran",
+		"Gino Appelbaum",
+		"Queen Lafountain",
+		"Eleni Witcher",
+		"Nola Macarthur",
+		"Jaessa Zahm",
+		"Elwood Spaulding",
+		"Dedra Kicklighter",
+		"Jere Gerdts",
+		"Matt Kaye",
+		"Thomes Elbert",
+		"Janeth Almanzar",
+		"Keren Liu",
+		"Emily Armor",
+		"Merissa Asaro",
+		"Zada Barter",
+		"Margot Minton"
+	];
+
+	$scope.submitRequest = function(request) {
+		$data.write.submitRequest(request).then(function(result) {
+		});
+	};
 }]);
