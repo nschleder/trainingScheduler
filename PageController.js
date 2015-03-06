fcc0dadfa0cac72c59abb45800c54961a76764d9
nschleder <nschleder9@gmail.com>
2015-03-06 08:57:49 -0800

    test2

--- PageController.js
+++ PageController.js
@@ -5,46 +5,5 @@ angular.module('trainingScheduler').controller('PageController', ['$http', '$sco
 		$scope.requests = resp.results;
 	});
 =======
-	$scope.emp = [
-		"John Smith",
-		"Micky Mouse",
-		"Luke Skywalker",
-		"John Shapered",
-		"Gordon Freemen",
-		"Lele Freewalker",
-		"Jessie SeeFar",
-		"Tarzan Jungal`Lard",
-		"Jerry Schossow",
-		"Nick Schleder",
-		"Ahn Tran",
-		"Gino Appelbaum",
-		"Queen Lafountain",
-		"Eleni Witcher",
-		"Nola Macarthur",
-		"Jaessa Zahm",
-		"Elwood Spaulding",
-		"Dedra Kicklighter",
-		"Jere Gerdts",
-		"Matt Kaye",
-		"Thomes Elbert",
-		"Janeth Almanzar",
-		"Keren Liu",
-		"Emily Armor",
-		"Merissa Asaro",
-		"Zada Barter",
-		"Margot Minton"
-	];
-	$scope.complete=function(){
-      console.log($scope.emp);
-    $( "#emp;" ).autocomplete({
-      source: $scope.emp
-    });
-    } 
-
-	
-	$scope.submitRequest = function(request) {
-		$data.write.submitRequest(request).then(function(result) {
-		});
-	};
 >>>>>>> origin/master
 }]);
